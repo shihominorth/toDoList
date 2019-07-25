85ad1467444ee12a8be832f1ffad1fb4f43c0673
Northisland <shihomi0104@icloud.com>
2019-07-25 15:50:36 -0700

    i can't show task line by line

---
+++ tag.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class tag {
+	writeTask tasks = new writeTask();
+	//ArrayList<String> arr = tasks.getArrTask();
+
+
+//	public void taskTable(ArrayList<String> arrTask) {
+//		
+//		for (int i = 0; i < arrTask.; i++) {
+//			System.out.println(arrTask.get(i));
+//		}
+//	}
+
+
+}
