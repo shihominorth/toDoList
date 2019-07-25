7ed0daaa4833789d6107e8aef7639231e73696a0
Northisland <shihomi0104@icloud.com>
2019-07-25 15:48:23 -0700

    first ver

---
+++ mainToDo.java
@@ -0,0 +1,13 @@
+
+
+public class mainToDo {
+	
+	static writeTask writeTask = new writeTask();
+	static tag tag = new tag();
+	
+	
+	public static void main(String[] args) {
+		writeTask.askTask();
+		
+	}
+}
